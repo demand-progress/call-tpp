eb7497dd10adc193869deb3cd525108c0fe4a665
Josh Levinger <josh@levinger.net>
2015-02-23 16:32:25 -0800

    new design

--- js/main.js
+++ js/main.js
@@ -1,18 +1,11 @@
 $(document).ready(function() {
-
-  $('#vortex').vortex({
-    initialPosition: 270,
-    speed: 150,
-    deepFactor: 0
-  });
-
-  $('#vortex, #act').hover(function() {
-    $('#vortex').data('vortex').stop();
-  }, function() {
-    $('#vortex').data('vortex').start();
-  });
-
-  $('#act').hover(function() {
-    $('input#phone').fadeIn();
-  });
+  circle = {
+      path: { radius: 125, center: [150, 35], angle: "180deg"},
+      targets: ".circle",
+      rotationMode: "rotate",
+      align: "center",
+      css: "visibility: visible; position: relative; top: 0px; width: 300px; height: 300px;",
+      // showPath: {thickness: 1, color: "white"},
+    };
+    cssWarp(circle);
 });
